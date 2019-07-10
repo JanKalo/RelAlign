2479097b073e313e9911d06b35869a6aa1b25bd8
Philipp Ehler <43780766+Mayo42@users.noreply.github.com>
2019-07-10 12:56:41 +0200

    Fixed baseline_FB15K script

--- baseline_FB15K.sh
+++ baseline_FB15K.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # FB15K_2000_50
+mkdir experiments/FB15K_2000_50_baseline
 python2 -m baseline benchmarks/FB15K_2000_50/train2id.txt experiments/FB15K_2000_50_baseline/synonyms_minSup_0.02.txt 0.02
 python3 -m baseline_evaluation experiments/FB15K_2000_50_baseline/synonyms_minSup_0.02.txt benchmarks/FB15K_2000_50/synonyms_id.txt
 
