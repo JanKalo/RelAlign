afa2f1e41b1f930bd3ca9c7e7ee50b8b3638ead2
Philipp Ehler <43780766+Mayo42@users.noreply.github.com>
2020-05-10 17:45:49 +0200

    Fixed prediction shape in HolE

--- thirdParty/OpenKE/models/HolE.py
+++ thirdParty/OpenKE/models/HolE.py
@@ -60,4 +60,4 @@ class HolE(Model):
 		predict_h_e = tf.nn.embedding_lookup(self.ent_embeddings, predict_h)
 		predict_t_e = tf.nn.embedding_lookup(self.ent_embeddings, predict_t)
 		predict_r_e = tf.nn.embedding_lookup(self.rel_embeddings, predict_r)
-		self.predict = tf.reduce_sum(self._calc(predict_h_e, predict_t_e, predict_r_e), 1, keep_dims = True)
+		self.predict = tf.reduce_sum(self._calc(predict_h_e, predict_t_e, predict_r_e), 1, keep_dims = False)
